b74120cf5aa2f28b1edd68d7c81223ed28c1f481
tryingMyHardest <dorana@Administrators-MacBook-Pro.local>
2023-05-05 14:37:47 -0400

    Updates to UI/UX on resume page

--- Resume/src/code.js
+++ Resume/src/code.js
@@ -1,13 +1,45 @@
-var nav = document.querySelector(".navBar");
+var title = document.querySelector(".title");
 
 window.onscroll = function () {handleScroll()};
 
 const handleScroll = () => {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    // Show nav
-    nav.classList.add("display");
-  } else {
-    // Hide nav
-    nav.classList.remove("display");
-  }
+   if (document.body.scrollTop > visualViewport.height || document.documentElement.scrollTop > visualViewport.height) {
+     title.classList.add("display");
+   } else {
+    title.classList.remove("display");
+   }
+ }
+
+
+
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    const circles = document.querySelector(".circles");
+    if(entry.isIntersecting){
+      let i = 1;
+      for(const child of circles.children){
+        if(i%2 == 0){
+          child.classList.add("bubbleEven");
+        }else{
+          child.classList.add("bubbleOdd");
+        }
+        i+=1;
+      }
+    }else{
+      let i = 1;
+      for(const child of circles.children){
+        if(i%2 == 0){
+          child.classList.remove("bubbleEven");
+        }else{
+          child.classList.remove("bubbleOdd");
+        }
+        i+=1;
+      }
+    }
+  })
 }
+
+let observer = new IntersectionObserver(callback);
+
+observer.observe(document.querySelector('header'));
